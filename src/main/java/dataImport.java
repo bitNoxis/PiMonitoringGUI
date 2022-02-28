209809b79c80d444ceb627a73280716bb9708e97
Tom Kunde <Tom.kunze11@gmx.de>
2022-02-28 13:08:39 +0100

    Auslesen

--- src/main/java/dataImport.java
+++ src/main/java/dataImport.java
@@ -1,27 +1,41 @@
 import java.sql.*;
 
+import static java.lang.Thread.sleep;
+
 public class dataImport{
 
-    public dataImport(){
-        String url = "jdbc:mysql://34.159.157.247/datenbank";
+    public dataImport() throws InterruptedException {
+        String url = "jdbc:mysql://34.159.157.247/monitoringDatenbank";
         String user = "root";
         String pass = "OA6I2K76r7BzEIGC";
-String cpuFrequenz1 ="";
-String cpuTemperatur1 = "";
-        try {
-            Connection con = DriverManager.getConnection(url, user, pass);
-            Statement stm = con.createStatement();
-            ResultSet rs = stm.executeQuery("select * from entries;");
+        String cpuFrequenz1 = "";
+        String cpuTemperatur1 = "";
+        while (true == true) {
+            sleep (10);
+            try {
+                Connection con = DriverManager.getConnection(url, user, pass);
+                Statement stm = con.createStatement();
+                ResultSet rs = stm.executeQuery("SELECT * FROM entries ORDER BY entryID DESC LIMIT 1;");
 
-
-            while(rs.next()){
-                System.out.println(rs.getString(1) + " " +
-                        rs.getString(2) + " " +
-                        rs.getString(3));
+                while (rs.next()) {
+                    //  System.out.println(rs.getString(1) + " " +
+                    //        rs.getString(2) + " " +
+                    //          rs.getString(3));
+                    cpuTemperatur1 = rs.getString(1);
+                    cpuFrequenz1 = rs.getString(2);
+                }
+                doWrite(cpuTemperatur1, cpuFrequenz1);
             }
 
-        } catch (SQLException e) {
+         catch(SQLException e){
             System.out.println(e.getMessage());
         }
+    }
+    }
+    private void doWrite(String cpuTemp, String cpuFrequenz){
+
+        System.out.println(cpuTemp);
+        System.out.println(cpuFrequenz);
+
     }
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -7,28 +7,12 @@ import java.sql.SQLException;
 
 
 public class main {
-        public static void main(String[] args){
-            new MonitoringPanel();
+        public static void main(String[] args) throws InterruptedException {
+           // new MonitoringPanel();
             String url = "jdbc:mysql://34.159.157.247/datenbank";
             String user = "root";
             String pass = "OA6I2K76r7BzEIGC";
+            new dataImport();
 
-            try {
-                Connection con = DriverManager.getConnection(url, user, pass);
-                Statement stm = con.createStatement();
-                ResultSet rs = stm.executeQuery("select * from entries;");
-
-
-                while(rs.next()){
-                    System.out.println(rs.getString(1) + " " +
-                            rs.getString(2) + " " +
-                            rs.getString(3));
-                }
-
-            } catch (SQLException e) {
-                System.out.println(e.getMessage());
-            }
         }
-
-
 }
