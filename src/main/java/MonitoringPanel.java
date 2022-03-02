750f9e90b540bc3db4a415e656eb4e01b34dd07a
Tom Kunde <Tom.kunze11@gmx.de>
2022-03-02 12:12:10 +0100

    klappt alles so

--- src/main/java/MonitoringPanel.java
+++ src/main/java/MonitoringPanel.java
@@ -7,7 +7,7 @@ public class MonitoringPanel extends JFrame {
     private JLabel labelCPUTemp1 = new JLabel("CPU Temperatur:");
     private JLabel labelCPUFrequenz1 = new JLabel("CPU GHz:");
     private JLabel labelCPUTemp2 = new JLabel("CPU Temperatur:");
-    private JLabel getLabelCPUFrequenz2 = new JLabel("CPU GHz:");
+    private JLabel labelCPUFrequenz2 = new JLabel("CPU GHz:");
 
     private TextField textboxCPUTemp1 = new TextField();
     private TextField textboxCPUFrequenz1 = new TextField();
@@ -40,11 +40,29 @@ public class MonitoringPanel extends JFrame {
         labelPI1.setFont(new Font("Dialog", Font.BOLD, 20));
         labelPI1.setBounds(100, 70, 100, 30);
 
+        labelCPUTemp2.setBounds(300,150,100,20);
+
+        textboxCPUTemp2.setEditable(false);
+        textboxCPUTemp2.setBounds(410,150,50,20);
+
+        labelCPUFrequenz2.setBounds(300,180,100,20);
+
+        labelPI2.setFont(new Font("Dialog", Font.BOLD, 20));
+        labelPI2.setBounds(390,70,100,20);
+
+        textboxCPUFrequenz2.setEditable(false);
+        textboxCPUFrequenz2.setBounds(410,180,50,20);
+
         frame.add(textboxCPUFrequenz1);
         frame.add(labelCPUFrequenz1);
         frame.add(textboxCPUTemp1);
         frame.add(labelCPUTemp1);
         frame.add(labelPI1);
+        frame.add(labelCPUFrequenz2);
+        frame.add(textboxCPUFrequenz2);
+        frame.add(textboxCPUTemp2);
+        frame.add(labelCPUTemp2);
+        frame.add(labelPI2);
         frame.add(labelTEst);
 
         return frame;
@@ -53,8 +71,8 @@ public class MonitoringPanel extends JFrame {
     public void doFill(String cpuTemp1, String cpuFrequenz1, String cpuTemp2, String cpuFrequenz2) {
         textboxCPUTemp1.setText(cpuTemp1);
         textboxCPUFrequenz1.setText(cpuFrequenz1);
-//textboxCPUTemp2.setText(cpuTemp2);
-//textboxCPUFrequenz2.setText(cpuFrequenz2);
+        textboxCPUTemp2.setText(cpuTemp2);
+        textboxCPUFrequenz2.setText(cpuFrequenz2);
 
 
     }
